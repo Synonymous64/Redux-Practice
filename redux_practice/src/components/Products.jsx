9916fbe0e12d418e3bd2631e82030ca60bd8ca13
Synonymous64 <prajwalurkude007@gmail.com>
2023-01-18 15:50:21 +0530

    productSlice dispatch useselectors api fetch from redux

--- redux_practice/src/components/Products.jsx
+++ redux_practice/src/components/Products.jsx
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../store/cartSlice';
+import { fetchProducts, STATUSES } from '../store/productSlice.js'
 const Products = () => {
+    // const [products, setProducts] = useState([]);
     const dispatch = useDispatch();
-    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        //! using Redux method of fetch API
+        dispatch(fetchProducts());
+        // const fetchProducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     console.log(data);
+        //     setProducts(data);
+        // }
+        // fetchProducts();
+    }, [])
+    const { data: products, status } = useSelector((state) => state.product);
     const handleAdd = (product) => {
         dispatch(add(product));
     }
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            console.log(data);
-            setProducts(data);
-        }
-        fetchProducts();
-    }, [])
+    if (status === STATUSES.LOADING) {
+        return <h2>Loading...</h2>;
+    }
+    if (status === STATUSES.ERROR) {
+        return <h2>Something went wrong</h2>
+    }
     return (
         <div className='productsWrapper'>
             {products.map((product) => (
--- redux_practice/src/store/productSlice.js
+++ redux_practice/src/store/productSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 //! This object is freeze, it means that we can not change object values. (READ-ONLY) 
-const STATUSES = Object.freeze({
+export const STATUSES = Object.freeze({
     IDLE: 'idle',
     ERROR: 'error',
     LOADING: 'loading',
@@ -16,18 +16,57 @@ const productSlice = createSlice({
     //* Reducers are used to mutate the state
     //* Reducers are pure function (the function which doesn't have any side effects, which means that it never changes the data of outside the function)  
     //* It simple terms they just change the state 
+    //! this reducers are used for commented thunk syntax 
     reducers: {
-        setProducts(state, action) {
-            //! DO NOT DO THIS -- NEVER (do not do async call from reducers as they are pure function) 
-            //^ const res = await fetch('https://fakestoreapi.com/products');
-            state.data = action.payload;
-        },
-        remove(state, action) {
-            return state.filter((item) => item.id !== action.payload);
-        },
+        // setProducts(state, action) {
+        //! DO NOT DO THIS -- NEVER (do not do async call from reducers as they are pure function) 
+        //     //^ const res = await fetch('https://fakestoreapi.com/products');
+        //     state.data = action.payload;
+        // },
+        // setStatus(state, action) {
+        //     state.status = action.payload;
+        // },
     },
+    //! by using async thunk method for first thunk syntax
+    extraReducers: (builder) => {
+        builder.addCase(fetchProducts.pending, (state, action) => {
+            state.status = STATUSES.LOADING;
+        }).addCase(fetchProducts.fulfilled, (state, action) => {
+            state.data = action.payload;
+            state.status = STATUSES.IDLE;
+        }).addCase(fetchProducts.rejected, (state, action) => {
+            state.status = STATUSES.ERROR;
+        })
+    }
 });
-export const { setProducts, remove } = productSlice.actions;
+export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
-// Thunks 
+//& the word thunk is a programming term which means "a peace of code that does some delayed work". Rather than execute some logic now, we can write a function body or code that can be used to perform the work later
+//! The thunks are used in two ways 
+
+//! Thunks is function itself which returns a function inside from it. 
+
+//! method 2 
+export const fetchProducts = createAsyncThunk('product/fetch', async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    return data
+})
+
+//! method 1
+ 
+// export function fetchProducts() {
+//     return async function fetchProductThunk(dispatch, getState) {
+//         dispatch(setStatus(STATUSES.LOADING));
+//         try {
+//             const res = await fetch('https://fakestoreapi.com/products');
+//             const data = await res.json();
+//             dispatch(setProducts(data));
+//             dispatch(setStatus(STATUSES.IDLE));
+//         } catch (err) {
+//             console.log(err);
+//             dispatch(setStatus(STATUSES.ERROR));
+//         }
+//     }
+// } 
